59d4f0b593808479c810579bdece6fee62576bff
kravcov.artemiy <kravcov.artemiy@wildberries.work>
2025-07-24 19:12:22 +0300

    add task 5

--- task_5/consumer.py
+++ task_5/consumer.py
@@ -1,33 +1,65 @@
 import sys
 import nats
 import json
+import signal
 import random
 import asyncio
-from asyncio import run
-from datetime import datetime
-from misc import SUBJECT, URL, get_logger
+from producer import get_logger
+from nats.js.api import ConsumerConfig
 
 
-async def main(_from, _to, _group):
-    nc = await nats.connect(URL)
-    sub = await nc.subscribe(SUBJECT, queue=_group)
-    logger = get_logger(_group)
+async def main(_from, _to, _name):
+    nc = await nats.connect([
+        'nats://localhost:4222', 
+        'nats://localhost:4223', 
+        'nats://localhost:4224'],
+        user='user',
+        password='user')
+    js = nc.jetstream()
+    logger = get_logger(_name)
     logger.info('Start consuming')
-
-    async for msg in sub.messages:
-        user_id = msg.headers['user_id']
-        if int(msg.headers['ts']) < int(datetime.now().timestamp()) - 2:
-            # оставляю 3 секунды на обработку сообщения. если ожидание уже 4, точно не успею
-            logger.warning(f'Skipping {user_id=}')
-            continue
-        data = json.loads(msg.data.decode())
-        logger.info(f"{user_id}, pending={sub.pending_msgs}")
-        response = {'group': _group, **data}
-        await asyncio.sleep(random.uniform(float(_from), float(_to)))
-        await msg.respond(json.dumps(response).encode())
+    sub = await js.pull_subscribe(
+        subject="users.requests", 
+        durable='workers',
+        config=ConsumerConfig(
+            ack_policy="explicit",
+            ack_wait=30,
+            max_ack_pending=1))
+        
+    user_id, msg = None, None
+    while True: 
+        try:
+            msgs = await sub.fetch(batch=1)
+            for msg in msgs:
+                user_id = msg.headers['user_id']
+                data = json.loads(msg.data.decode())
+                logger.info(f"Took {user_id}")
+                response = {'group': _name, **data}
+                await asyncio.sleep(random.uniform(float(_from), float(_to)))
+                await nc.publish(
+                    msg.headers['reply_to'], 
+                    json.dumps(response).encode(),
+                    headers={'user_id': user_id})                
+                await msg.ack()
+        except nats.errors.TimeoutError:
+            logger.info('Empty queue')
+        except asyncio.CancelledError:
+            logger.info(f'Graceful shutdown on {user_id=}')
+            if msg and msg.is_acked is False:
+                await msg.nak()
+            await nc.close()
+            break
+        except Exception as exc:
+            logger.exception(exc)
+            await nc.close()
+            raise exc
 
 
 if __name__ == '__main__':
     _from, _to = sys.argv[1:3] if len(sys.argv) >= 3 else (2, 3)
-    _group = sys.argv[3] if len(sys.argv) >= 4 else 'workers'
-    run(main(_from, _to, _group))
+    _name = sys.argv[3] if len(sys.argv) >= 4 else 'workers'
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)    
+    main_task = loop.create_task(main(_from, _to, _name))
+    signal.signal(signal.SIGINT, lambda a, b: main_task.cancel())
+    loop.run_until_complete(main_task)
--- task_5/producer.py
+++ task_5/producer.py
@@ -1,52 +1,122 @@
 import sys
 import nats
 import json
+import signal
 import random
-from datetime import datetime
 import asyncio
+import logging
+from uuid import uuid4
+from datetime import datetime
+from functools import partial
 from itertools import cycle
-from asyncio import create_task, Event, sleep, run
-from nats.errors import NoRespondersError, TimeoutError
-from misc import SUBJECT, URL, get_logger
+from asyncio import create_task
+from nats.errors import TimeoutError
+from nats.js.api import ConsumerConfig
 
 
-async def get_request(nc, message, no_consumers_event, logger):
+def get_logger(name):
+    LOG_FMT = "%(asctime)s | %(levelname)s | %(name)s | %(message)s"
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.DEBUG)
+    console_handler = logging.StreamHandler(sys.stdout)
+    console_handler.setFormatter(logging.Formatter(LOG_FMT))
+    logger.addHandler(console_handler) 
+    return logger
+
+
+async def make_request(nc, js, _name, message, logger):
     user_id = str(message['user_id'])
-    ts = str(int(datetime.now().timestamp()))
-    headers = dict(user_id=user_id, ts=ts)
+    reply_to = f"users.responses.{user_id}"
+    headers = dict(user_id=user_id, reply_to=reply_to)
+    sub = None
     try:
         logger.info(f'Published {user_id=}')
-        response = await nc.request(
-            SUBJECT, 
+        await nc.publish(
+            'users.requests', 
             json.dumps(message).encode(),
-            timeout=5, 
             headers=headers)
-        group = json.loads(response.data).get('group')
-    except NoRespondersError:
-        no_consumers_event.set()
+        sub = await js.pull_subscribe(
+            subject=reply_to, 
+            durable='producers', 
+            config=ConsumerConfig(
+                durable_name='producers',
+                ack_policy="explicit"))
+        msg = (await sub.fetch(batch=1, timeout=5))[0]
+        group = json.loads(msg.data).get('group')
+        await msg.ack()
+        logger.info(f"Received response for {user_id=}: {group=}") 
+    except asyncio.CancelledError:
+        logger.info(f"{user_id=} => 'user.on_broker_shutdown'")
+        await nc.publish(
+            'user.on_producer_shutdown', 
+            json.dumps(message).encode(), 
+            headers=headers)
     except TimeoutError:
         logger.error(f'Timeout for {user_id=}')
 
 
-async def main(_group, _multiplier):
-    nc = await nats.connect(URL)
-    no_consumers_event = Event()
-    logger = get_logger(_group)
+async def listen_to_other_producers_shutdown(is_cancelled, nc, js, _name, logger, tasks):
+    try:
+        sub = await nc.subscribe(
+            'user.on_producer_shutdown', 
+            queue='on_producer_shutdown')
+        async for msg in sub.messages:
+            task_id = str(uuid4())
+            message = json.loads(msg.data.decode())
+            user_id = message['user_id']
+            logger.info(f"'user.on_broker_shutdown' => {user_id=}")
+            tasks[task_id] = create_task(make_request(nc, js, _name, message, logger))
+            rm_from_tasks = partial(lambda d, k, _: d.pop(k), tasks, task_id)
+            tasks[task_id].add_done_callback(rm_from_tasks)
+    except asyncio.CancelledError:
+        await sub.unsubscribe()
+        is_cancelled.set()
+    except Exception as exc:
+        logger.exception(exc)
+
+
+async def main(_name, _multiplier):
+    nc = await nats.connect([
+        'nats://localhost:4222', 
+        'nats://localhost:4223', 
+        'nats://localhost:4224'],
+        user='user',
+        password='user')
+    js = nc.jetstream()
+    logger = get_logger(_name)
     users_bank = cycle(range(1000))
+    tasks = dict()
+    listen_cancelled = asyncio.Event()
+    listen_task = create_task(listen_to_other_producers_shutdown(
+        listen_cancelled, nc, js, _name, logger, tasks))
     try:
         while True:
-            if no_consumers_event.is_set():
-                logger.warning('No consumers available. Falling asleep')
-                await sleep(10)
-                no_consumers_event.clear()
             user_id = next(users_bank) * int(_multiplier)
-            message = {"user_id": user_id}
-            create_task(get_request(nc, message, no_consumers_event, logger))
+            ts = str(int(datetime.now().timestamp()))
+            task_id = str(uuid4())
+            message = {"user_id": user_id, "ts": ts}
+            tasks[task_id] = create_task(make_request(nc, js, _name, message, logger))
+            rm_from_tasks = partial(lambda d, k, _: d.pop(k), tasks, task_id)
+            tasks[task_id].add_done_callback(rm_from_tasks)
             await asyncio.sleep(random.uniform(0, 1.5))
+    except asyncio.CancelledError:
+        listen_task.cancel()
+        logger.info('Graceful shutdown')
+        # нельзя продолжать без отписки от on_producer_shutdown, иначе сообщения вернутся
+        await listen_cancelled.wait()
+        for task in tasks.values():
+            task.cancel()
+        while len(tasks) > 0:
+            await asyncio.sleep(0.1)
     finally:
         await nc.close()
 
+
 if __name__ == '__main__':
-    _group = sys.argv[1] if len(sys.argv) >= 2 else 'producer'
+    _name = sys.argv[1] if len(sys.argv) >= 2 else 'producer'
     _multiplier = sys.argv[2] if len(sys.argv) == 3 else 1
-    run(main(_group, _multiplier))
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)    
+    main_task = loop.create_task(main(_name, _multiplier))
+    signal.signal(signal.SIGINT, lambda a, b: main_task.cancel())
+    loop.run_until_complete(main_task)
